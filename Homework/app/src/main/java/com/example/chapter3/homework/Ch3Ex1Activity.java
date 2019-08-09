cf6d73427bc2587f35d1111776309dc38fff6ef3
aceprcet <aceprcet@126.com>
2019-08-09 19:38:21 +0800

    使用Lottie实现作业一，并且使用ValueAnimator进行设置

--- Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex1Activity.java
+++ Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex1Activity.java
@@ -1,5 +1,6 @@
 package com.example.chapter3.homework;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CheckBox;
@@ -22,16 +23,27 @@ public class Ch3Ex1Activity extends AppCompatActivity {
         loopCheckBox = findViewById(R.id.loop_checkbox);
         seekBar = findViewById(R.id.seekbar);
 
+        ValueAnimator animator = ValueAnimator.ofFloat(0, 1);
+        animator.setDuration(1000);
+        animator.setRepeatMode(ValueAnimator.RESTART);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.addUpdateListener(animation -> {
+            animationView.setProgress((Float) animation.getAnimatedValue());
+            seekBar.setProgress((int) ((Float) animation.getAnimatedValue() * 100));
+        });
+
         loopCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     // 当选中自动播放的时候，开始播放 lottie 动画，同时禁止手动修改进度
-                    animationView.playAnimation();
+                    // animationView.playAnimation();
+                    animator.start();
                     seekBar.setEnabled(false);
                 } else {
                     // 当去除自动播放时，停止播放 lottie 动画，同时允许手动修改进度
-                    animationView.pauseAnimation();
+                    // animationView.pauseAnimation();
+                    animator.pause();
                     seekBar.setEnabled(true);
                 }
             }
@@ -40,6 +52,11 @@ public class Ch3Ex1Activity extends AppCompatActivity {
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                float value = (float) progress / 100;
+//                animationView.setProgress(value);
+                if (fromUser) {
+                    animator.setCurrentFraction((float) progress / 100);
+                }
                 // TODO ex1-2: 这里应该调用哪个函数呢
                 // 提示1：可以参考 https://airbnb.io/lottie/#/android?id=custom-animators
                 // 提示2：SeekBar 的文档可以把鼠标放在 OnProgressChanged 中间，并点击 F1 查看，
